b688ec08b4fec5cf0580f7c2dead47a725ddab83
Douae sb <douae.sb411@example.com>
2024-02-05 17:18:36 +0100

    laravel struct

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('photo');
+            $table->string('industrie');
+            $table->string('titre');
+            $table->string('email')->unique();
+            $table->string('poste');
+            $table->string('tel');
+            $table->string('adresse');
+            $table->string('description');
+            $table->string('role');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
